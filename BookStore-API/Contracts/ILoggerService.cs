1583e74a729e75b539395dd66c2b367eec4593d6
Md. Ziaul Hoque <mdziahoq@gmail.com>
2021-07-27 19:16:57 +0600

    Add project files.

---
+++ BookStore-API/Contracts/ILoggerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Contracts
+{
+    public interface ILoggerService
+    {
+        void LogInfo(String message);
+
+        void LogWarn(String message);
+
+        void LogDebug(String message);
+
+        void LogError(string message);
+    }
+}
